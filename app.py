a6bcaee419e56e6ce962163f69e669d911a0461b
Karola-ING <108148869+Karola-ING@users.noreply.github.com>
2024-03-03 15:09:03 +0100

    Connect Database

--- app.py
+++ app.py
@@ -3,36 +3,39 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 JOBS = [
-    {
-        'id': 1,
-        'title': 'IT Desktop Support Junior Specialist',
-        'location': 'Cracow, Małopolskie, Poland',
-        'company': 'Zurich Insurance',
-        'salary': '10,000 PLN'
-    },
-    {
-        'id': 2,
-        'title': 'IT Service Support Analyst',
-        'location': 'Gniezno, Wielkopolskie, Poland',
-        'company': 'LKQ Europe',
-        'salary': '7,000 PLN'
-    },
-    {
-        'id': 3,
-        'title': 'Administrator IT',
-        'location': 'Wrocław, Dolnośląskie, Poland',
-        'company': 'Polska Grupa Farmaceutyczna S.A.',
-        'salary': '5,000 PLN'
-    }
+  {
+    'id': 1,
+    'title': 'IT Desktop Support Junior Specialist',
+    'location': 'Cracow, Małopolskie, Poland',
+    'company': 'Zurich Insurance',
+    'salary': '10,000 PLN'
+  },
+  {
+    'id': 2,
+    'title': 'IT Service Support Analyst',
+    'location': 'Gniezno, Wielkopolskie, Poland',
+    'company': 'LKQ Europe',
+    'salary': '7,000 PLN'
+  },
+  {
+    'id': 3,
+    'title': 'Administrator IT',
+    'location': 'Wrocław, Dolnośląskie, Poland',
+    'company': 'Polska Grupa Farmaceutyczna S.A.',
+    'salary': '5,000 PLN'
+  }
 ]
+
 
 @app.route('/')
 def home():
     return render_template('jobs.html', jobs=JOBS)
 
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8000)
---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE jobs (
+  job_id SERIAL PRIMARY KEY,
+  job_title VARCHAR (50) NOT NULL,
+  job_location VARCHAR (255) NOT NULL,
+  salary int,
+  currency VARCHAR(10),
+  responsibilities VARCHAR(2000),
+  requirements VARCHAR(2000)
+);
+
+INSERT INTO jobs (job_title, job_location, salary, currency, responsibilities, requirements)
+VALUES  ('Frontend Developer','Remote', 12000, 'PLN','Translate designs and wireframes into high quality JS, CSS, HTML templates
+Design, build, and maintain high performance, reusable, and reliable UI components and products
+Ensure the best possible performance, quality, and optimize for maximum speed and scalability
+Identify and correct bottlenecks and fix bugs.
+Help maintain code quality, organization, and automatization.','Strong knowledge of programming skills in JS, CSS and HTML
+Familiarity with responsive and adaptive web design, and good knowledge of JS libraries such as JQuery
+Strong knowledge of about atleast one of the JS frameworks (e.g. VueJS, Angular JS, NodeJS, ReactJS)
+Experience with building websites, ability to handle cross browser compatibility issues'),
+    ('IT Service Support Analyst','Gniezno, Wielkopolskie, Poland', 7000, 'PLN','Draft detailed scope for assigned projects, addressing suggested methodology and execution framework.
+Execute on the plan with appropriate data mining, analytical and data science techniques.
+Perform quality assurance of data and deliverables for work performed by other Data Scientists, Data Engineers and self.
+Accountable for the quality of the end solution or model by planning the required reviews on code and process.','Expert knowledge in Deep Learning techniques and exploring newer approaches like federated learning and transfer learning.
+Proficient in some or all of the following techniques: Linear & Logistic Regression, Decision Trees, Random Forests, K-Nearest Neighbors, Markov Chain, Monte Carlo, Gibbs Sampling, Evolutionary Algorithms, Support Vector Machines.
+Proficient in advanced data mining and statistical modeling techniques, including Predictive modeling'),
+    ('Backend Developer','Wrocław, Dolnośląskie, Poland', 18000, 'PLN','Design and develop a cloud based backend
+Participate and produce a scalable cloud based backend system
+Design and develop REST based APIs
+Interact with customer directly and with other stakeholders in the organization','Hands on experience with building a web backend in Java or Golang
+Knowledge of designing and building REST APIs
+Proven experience in building a scalable and resilient backend
+Good understanding of database schemas and using both relational (SQL) and noSQL based data stores
+Strong analytical and debugging skills');
